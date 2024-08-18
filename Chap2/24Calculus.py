11bbb930c50b8f46c8b4d22d1eb3c58f08c3107a
LabAnton <anton-sinner@gmx.de>
2024-08-18 12:31:59 +0200

    More 2.4

--- Chap2/24Calculus.py
+++ Chap2/24Calculus.py
@@ -19,7 +19,35 @@ plt.plot(a, f(a), "b")
 plt.show()
 
 #Exercises
-#1.So far we took the rules for derivatives for granted. Using the definition and limits prove the properties for (i) f(x) = c, (ii) f(x) = x**n, (iii) f(x) = e**x and (iv) f(x) = log(x)
-# (i) f'(x) = lim h->0 (c-c)/h = 0 (ii) f'(x) = lim h->0 ((x+h)**n-x**n)/h use induction case n=1: lim h->0 (x+h-x)/h = 1 n: f'(x) = nx**(n-1) N=n+1: f'(x) = lim h->0 ((x+h)**(N)-x**N)/h = lim h->0 ((x+h)**n*(x+h)-x**n*x)/h = lim h->0 (x*((x+h)*n-x*n)+h*(x+h)**n)/h = n*x**n+x**n = N*x**n (iii) f'(x) = lim h->0 (e**(x+h)-e**x)/h = lim h->0 e**x * (h-1)/h = lim h->0 e**x - 1/h = e**x (iv) f'(x) = lim h->0 (log(x+h)-log(x))/h = lim h->0 log((x+h)/x)/h = log(1+h/x)/h
 
+#1. So far we took the rules for derivatives for granted. Using the definition and limits prove the properties for (i) f(x) = c, (ii) f(x) = x**n, (iii) f(x) = e**x and (iv) f(x) = log(x)
+# (i) f'(x) = lim h->0 (c-c)/h = 0 (ii) f'(x) = lim h->0 ((x+h)**n-x**n)/h use induction case n=1: lim h->0 (x+h-x)/h = 1 n: f'(x) = nx**(n-1) N=n+1: f'(x) = lim h->0 ((x+h)**(N)-x**N)/h = lim h->0 ((x+h)**n*(x+h)-x**n*x)/h = lim h->0 (x*((x+h)*n-x*n)+h*(x+h)**n)/h = n*x**n+x**n = N*x**n (iii) f'(x) = lim h->0 (e**(x+h)-e**x)/h = lim h->0 e**x * (h-1)/h = lim h->0 e**x - 1/h = e**x (iv) f'(x) = lim h->0 (log(x+h)-log(x))/h = lim h->0 log((x+h)/x)/h = lim h->0 log(1+h/x)/h = lim h->0 log(1+t)/tx = 1/x lim h->0 log((1+t)^(1/t)) -> 1/x log(e) -> depending on the logarithm we get 1/(x* ln(b)), where b is the base of the log
+
+#2. In the same vein, prove the product, sum, and quotient rule from first principles.
+#(i) (f(x)+g(x))' = lim h->0 (f(x+h)+g(x+h)-f(x)-g(x))/h = lim h->0 (f(x+h)-f(x))/h + (g(x+h)-g(x))/h = f'(x)+g'(x) (ii) (f(x)*g(x))' = lim h->0 (f(x+h)*g(x+h)-f(x)*g(x))/h = lim h->0 (f(x+h)*g(x+h)-g(x)f(x)+g(x)*f(x+h)-g(x)*f(x+h))/h = lim h->0 (g(x)*(f(x+h)-f(x)))/h + (f(x+h)*(g(x+h)*g(x)))/h = g(x)*f'(x)+ f(x)*g'(x) (iii) (f(x)/g(x))' = lim h->0 (f(x+h)/g(x+h)-f(x)/g(x))/h = lim h->0 1/h( (f(x+h)*g(x))/(g(x+h)*g(x)) - (f(x)*g(x+h))/(g(x)+g(x+h)) + (f(x)*g(x))/g(x)^2 - (f(x)*g(x))/g(x)^2) = lim h->0 1/h (((f(x+h)-f(x))*g(x))/g(x)^2 - ((g(x+h)-g(x))*f(x))/g(x)^2) = (f'(x)g(x)-f(x)g'(x))/g(x)^2
+
+#3. Prove that the constant multiple rule follows as a special case of the product rule.
+#(C*f(x))' = C'*f(x)+C*f'(x) = C*f'(x)
+
+#4. Calculate the derivative of f(x) = x^x
+#(x^x)' = (e^(x*ln(x)))' = e^(x*ln(x)) * (x*ln(x))' = x^x * (ln(x)+1)
+
+#5. What does it mean that f'(x)=0 for some x? Give an example of a function f and a location x for which this might hold.
+#Minima, Maxima or plateau. Exemplary functions are x^2 or x^3
+
+#6. Plot the function g(x) = x^3-1/x and plot its tangent line at x=1.
+def g(x): 
+    return x ** 3 - 1/x
+def tg(x):
+    return 4 * x - 4
+
+plt.plot(a, g(a), "r")
+plt.plot(a, tg(a), "b")
+plt.show()
+
+#7 Find the gradient of the function f(x) = 3x_1^2+5e^x_2
+# Derivative df(x)/dx_1 = 6x_1; df(x)/d_x2 = 5e^x_2 -> can be rewritten as gradient vector
+
+#8 What is the gradient of the function f(x) = ||x||_2? What happens for x=0?
+#Assume multiple x
 
